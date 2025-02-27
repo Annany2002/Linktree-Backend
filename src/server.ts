1de6d90d19b5528e3349de85245329ef4514c031
Annany <annivish2002@gmail.com>
2025-02-27 21:53:17 +0530

    Minor changes

--- src/server.ts
+++ src/server.ts
@@ -5,3 +5,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port: ${PORT}`);
 });
+
+app.get("/", (req, res) => {
+  res.send("Hello from server");
+});
